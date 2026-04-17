3c5229e2eec9869612efb05c8ed6faca58dc3870
Colin <colinanddad833@gmail.com>
2026-04-16 21:10:26 -0400

    fix syntax error

--- commands/removetransaction.js
+++ commands/removetransaction.js
@@ -1,27 +1,4 @@
 const { SlashCommandBuilder } = require('discord.js');
-const fs = require('node:fs');
-const path = require('node:path');
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('removetransaction')
-        .setDescription('Remove a transaction by Order ID')
-        .addIntegerOption(option => option.setName('id').setDescription('The Order ID').setRequired(true)),
-
-    async execute(interaction) {
-        const id = interaction.options.getInteger('id');
-        const filePath = path.join(__dirname, '..', 'data', 'transactions.json');
-        const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
-
-        const index = data.transactions.findIndex(t => t.id === id);
-        if (index === -1) return interaction.reply(`Order #${id} not found.`);
-
-        data.transactions.splice(index, 1);
-        fs.writeFileSync(filePath, JSON.stringify(data, null, 2));
-
-        await interaction.reply(`✅ Order #${id} has been removed successfully.`);
-    },
-};const { SlashCommandBuilder } = require('discord.js');
 const { Transaction, UserStats } = require('../models/Transaction');
 
 module.exports = {
